0a1fd5b298c660d5bacae4592cfa4ad905f7ef8f
n3m0-ke <michaelodhiambo38@gmail.com>
2025-09-23 18:16:37 +0300

    Admin side - dashboard page UI/UX improvement

--- components/dashboard/skeletons.tsx
+++ components/dashboard/skeletons.tsx
@@ -38,15 +38,28 @@ export function CardsSkeleton() {
         // 🔄 Shimmer state
         return (
             <>
-                <div>
-                    <Image
-                        src="/loding.gif"
-                        alt="loading..."
-                        width={24}
-                        height={24}
-                        className="mr-2"
-                    />
-                    Fetching Metrics ...
+                <div className="flex items-center space-x-3 text-teal-400">
+                    <svg
+                        className="animate-spin h-6 w-6 text-teal-400"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                        />
+                        <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                        />
+                    </svg>
+                    <span className="text-lg">Fetching data...</span>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                     {[1, 2, 3].map((i) => (
